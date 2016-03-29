48e8084be26b891cf40ba7c04e98ae430bb1f1cf
Chase Brasfield <chasebrasfield@gmail.com>
2016-03-29 16:46:05 -0500

    Project complete.

---
+++ client/js/controllers.js
@@ -0,0 +1,66 @@
+
+var controllers = angular.module('myApp.controllers', []);
+controllers.controller('WelcomeController', ['$scope', '$rootScope', '$http', function($scope, $rootScope, $http) {
+
+    $scope.op1 = null;
+    $scope.op2 = null;
+    $scope.answer = null;
+    $scope.alert = null;
+
+    $scope.add = function() {
+        if ($scope.op1 === null) {
+            $scope.alert = 'INPUT MISSING';
+        } else if ($scope.op2 === null) {
+            $scope.alert = 'INPUT MISSING';
+        } else {
+            $scope.answer = ($scope.op1 + $scope.op2)
+            $scope.op1 = null;
+            $scope.op2 = null;
+            $scope.alert = null;
+        }
+    };
+
+    $scope.subtract = function() {
+        if ($scope.op1 === null) {
+            $scope.alert = 'INPUT MISSING';
+        } else if ($scope.op2 === null) {
+            $scope.alert = 'INPUT MISSING';
+        } else {
+            $scope.answer = ($scope.op1 - $scope.op2)
+            $scope.op1 = null;
+            $scope.op2 = null;
+            $scope.alert = null;
+        }
+    };
+
+    $scope.multiply = function() {
+        if ($scope.op1 === null) {
+            $scope.alert = 'INPUT MISSING';
+        } else if ($scope.op2 === null) {
+            $scope.alert = 'INPUT MISSING';
+        } else {
+            $scope.answer = ($scope.op1 * $scope.op2)
+            $scope.op1 = null;
+            $scope.op2 = null;
+            $scope.alert = null;
+        }
+    };
+
+    $scope.divide = function() {
+        if ($scope.op1 === null) {
+            $scope.alert = 'INPUT MISSING';
+        } else if ($scope.op2 === null) {
+            $scope.alert = 'INPUT MISSING';
+        } else if ($scope.op1 === 0) {
+            $scope.alert = 'CAN NOT DIVIDE BY ZERO';
+        } else {
+            $scope.answer = ($scope.op1 / $scope.op2)
+            $scope.op1 = null;
+            $scope.op2 = null;
+            $scope.alert = null;
+        }
+    };
+
+
+
+}]);
---
+++ client/js/main.js
@@ -0,0 +1,10 @@
+
+var app = angular.module('myApp', ['myApp.controllers', 'ngRoute']);
+app.config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'views/welcome.html',
+            controller: 'WelcomeController'
+        });
+}]);
+
